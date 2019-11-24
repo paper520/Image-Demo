e8f26ffd756e4d5f3ee8310bff2f9ed2f7860a7f
心叶 <yelloxing@gmail.com>
2019-11-24 16:51:10 +0800

    添加首页说明

--- html/menu/mod.js
+++ html/menu/mod.js
@@ -1,7 +1,14 @@
 ctrlapp.register.controller('menuController', ['$scope', '$state', function ($scope, $state) {
-  
+
   $scope.initMethod = function () {
 
+    // 格式化代码
+    prettyPrint();
+
+  };
+
+  $scope.openDoc = function () {
+    window.location.href = "https://yelloxing.github.io/image2D/#/guide"
   };
 
 }]);
